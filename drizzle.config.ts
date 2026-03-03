2b1d7991ebed278ffad4bfda98c300e280f4782f
cavaleiro802-oss <cavaleiro802@gmail.com>
2026-03-02 22:21:51 -0300

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 
+const url = process.env.DATABASE_URL ||
+  `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT || "3306"}/${process.env.MYSQLDATABASE}`;
+
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle/migrations",
   dialect: "mysql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
+  dbCredentials: { url },
 });
