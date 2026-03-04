a2b0f9167cf5120a4151a177d2720b7f45ca41d9
cavaleiro802-oss <cavaleiro802@gmail.com>
2026-03-04 16:02:54 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -55,13 +55,20 @@ app.use(helmet({
   crossOriginEmbedderPolicy: false,
 contentSecurityPolicy: isProd ? {
 directives: {
+
 defaultSrc: ["'self'"],
+
 connectSrc: ["'self'", SITE_URL, "https://accounts.google.com", "https://oauth2.googleapis.com"].filter(Boolean),
+
 imgSrc: ["'self'", "data:", "https://*.r2.dev", "https://lh3.googleusercontent.com"],
+
 scriptSrc: ["'self'", "'unsafe-inline'", "https://accounts.google.com", "https://apis.google.com"],
+
 scriptSrcElem: ["'self'", "'unsafe-inline'", "https://accounts.google.com", "https://apis.google.com"],
+
 frameSrc: ["'self'", "https://accounts.google.com"],
-styleSrc: ["'self'", "'unsafe-inline'"],
+
+styleSrc: ["'self'", "'unsafe-inline'", "https://accounts.google.com"],
 },
 } : false
 }));
